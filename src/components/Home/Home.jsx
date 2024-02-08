18f7732be3c2db41698f71cee93ee60d8b6d61bc
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-08 21:54:40 +0600

    load data from public folder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+     const tshirts = useLoaderData();
+     console.log(tshirts);
      return (
           <div>
-               <h2>This is Home.</h2>
+               <h2>This is Home: {tshirts.length}</h2>
           </div>
      );
 };
