de0ff9830d032fab4db74d23cf5d8eebe2d22f69
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-09 23:48:43 +0600

    show message using conditional rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+
+     let message;
+     if (cart.length === 0) {
+          message = <p>Please add some product.
+          </p>
+     }
+     else {
+          message = <div>
+               <h3>Boroloxxx</h3>
+               <p>Thanks for westing your money.</p>
+          </div>
+     }
      return (
           <div>
                <h2>Order summary: {cart.length}</h2>
+               {message}
                {
                     cart.map(tshirt => <p
                          key={tshirt._id}>
